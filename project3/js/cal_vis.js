315dc24c0ff0112f9b124ba591a83e64f47b0bd7
Uchenna Chima <ifychim@gmail.com>
2021-03-21 23:15:25 -0400

    Add files via upload

--- project3/js/cal_vis.js
+++ project3/js/cal_vis.js
@@ -1,4 +1,5 @@
-function load_iris_visualization1(svg_name, data){
+
+function load_calvin_visualization1(svg_name, data, avg_satm, avg_satv){
 
     //console.log("HEREREER");
     let chart = d3.select(svg_name);
@@ -6,30 +7,27 @@ function load_iris_visualization1(svg_name, data){
     let width = $(svg_name).width() - margin2;
     let height = $(svg_name).height()- margin2;
 
-    
-
     //grouping
     //let g = chart.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     let g = chart.append("g")
                 .attr("transform", "translate(" + 55 + "," + 3 + ")");
 
-    
     //Adding Label to chart (title)
     g.append("text")
         .attr("text-anchor", "end")
         .attr("font-size", "25px")
         .attr("font-weight", "bold")
-        .attr("x", 550)
+        .attr("x", 650)
         .attr("y", 20)
-        .text("Iris Plant Robustness");
-              
-
+        .text("SATM vs SATV Average Scores");
+            
     //x,y scales
     let x = d3.scaleBand()
                 .range([0, width])//output
-                //.domain([0, data.length-1])//change this
-                .domain(data.map(function(data) { return data.class; }))//input
+                //.domain(function(d))
+                .domain(["SATM", "SATV"])//input
                 .padding(0.35);
+    
 
     //Appending x-axis to chart and labeling it 
     g.append("g")
@@ -41,24 +39,25 @@ function load_iris_visualization1(svg_name, data){
         .attr("font-size", "15px")
         .attr("font-weight", "bold")
         .attr("transform", "rotate(-90)")
-        .attr("x", -height + 350)
+        .attr("x", -height + 250)
         .attr("y", -40)
-        .text("Average Plant Robustness (CM)");
+        .text("Average Score");
         
     let y = d3.scaleLinear()
                 .range([height, 0]) //output
-                .domain([0,20]); //input
+                .domain([300,850]); //input
     
     //Appending y-axis to chart and labeling it 
     g.append("g").call(d3.axisLeft(y));
 
     g.append("text")
+        .attr("class", "ylabel")
         .attr("text-anchor", "end")
         .attr("font-size", "15px")
         .attr("font-weight", "bold")
         .attr("x", width-400)
         .attr("y", height+35)
-        .text("Iris Plant Class");
+        .text("Test Category");
 
     //console.log((data.map(function(data) { return data})));
     
@@ -67,68 +66,53 @@ function load_iris_visualization1(svg_name, data){
          .data(data)
          .enter()
          .append("rect")
-            .attr("x", function(d) { return x(d.class); })
-            .attr("y", function(d) { return y(0); }) // no bars at the start
-            .attr("width", x.bandwidth())
-            .attr("height", function(d) { return  height - y(0); }) // no bars at the start
-            .style("fill", "#000000")//filling each bar with black color;
+            .attr("x", 190)
+            .attr("class", function (d) { return "satmbar"} )
+            .attr("y", height-260)
+            .attr("width", 100)
+            .attr("height", 260)
+            .attr("fill", "#000000")//filling SATM bar with indigo color;*/
             .on("mouseover", mouseover_event)
             .on("mouseout",mouseout_event)
+        
+    g.selectAll("bar-graph")
+            .data(data)
+            .enter()
+            .append("rect")
+               .attr("x", 555)
+               .attr("class", function (d) { return "satvbar"} )
+               .attr("y", height-250)
+               .attr("width", 100)
+               .attr("height", 250)
+               .style("fill", "#000000")//filling SATV bar with some color
+               .on("mouseover", mouseover_event)
+               .on("mouseout",mouseout_event)
 
-    //Interactivity (FUNCTIONS AND ANIMATIONS)
-
-    //Animation (Display Bar Chart)
-
-    g.selectAll("rect")
-        .transition()
-        .duration(1700)
-        .attr("y", function(d){return y(d.robustness)})
-        .attr("height", function(d) { return  height- y(d.robustness); })
-    
+    //Interactivity (FUNCTIONS)
 
     g.append("text")
         .attr("text-anchor", "end")
         .attr("font-size", "15px")
         .attr("font-weight", "bold")
         .style("fill", "#000000")
-        .attr("x", width-630)
-        .attr("y", height-110)
-        .text(data[0].robustness + " CM");
-    
+        .attr("x", width-590)
+        .attr("y", height-150)
+        .text(Math.round(avg_satm));
+
     g.append("text")
         .attr("text-anchor", "end")
         .attr("font-size", "15px")
         .attr("font-weight", "bold")
         .style("fill", "#000000")
-        .attr("x", width-380)
+        .attr("x", width-225)
         .attr("y", height-130)
-        .text(data[1].robustness + " CM");
-
-    g.append("text")
-        .attr("text-anchor", "end")
-        .attr("font-size", "15px")
-        .attr("font-weight", "bold")
-        .style("fill", "#000000")
-        .attr("x", width-380)
-        .attr("y", height-130)
-        .text(data[1].robustness + " CM");
-    
-    g.append("text")
-        .attr("text-anchor", "end")
-        .attr("font-size", "15px")
-        .attr("font-weight", "bold")
-        .style("fill", "#000000")
-        .attr("x", width-135)
-        .attr("y", height-190)
-        .text(data[2].robustness + " CM");
-    
+        .text(Math.round(avg_satv));
 
     function mouseover_event(d, i){
         d3.select(this)
             .style("fill", "orange");
 	}
 
-    
 	function mouseout_event(d, i){
 		d3.select(this)
             .transition()
@@ -138,8 +122,8 @@ function load_iris_visualization1(svg_name, data){
            
 }
 
-function load_iris_visualization2(svg_name, data){
-
+function load_calvin_visualization2(svg_name, data, tip){
+    
     let chart = d3.select(svg_name);
     let margin2 = 50;
     let width = $(svg_name).width() - margin2;
@@ -152,16 +136,16 @@ function load_iris_visualization2(svg_name, data){
     //Adding Label to chart (title)
     g.append("text")
         .attr("text-anchor", "end")
-        .attr("font-size", "25px")
+        .attr("font-size", "15px")
         .attr("font-weight", "bold")
-        .attr("x", 575)
-        .attr("y", 20)
-        .text("Sepal Width vs Petal Width");
+        .attr("x", 200)
+        .attr("y", 10)
+        .text("Student GPA Distribution");
 
     //x,y scales
     let x = d3.scaleLinear()
                 .range([0, width])//output
-                .domain([2*0.95, 5]);//input
+                .domain([0, 280]);//input
     
     //Appending x-axis to chart and labeling it 
     g.append("g")
@@ -175,11 +159,11 @@ function load_iris_visualization2(svg_name, data){
         .attr("font-weight", "bold")
         .attr("x", width-400)
         .attr("y", height+35)
-        .text("Sepal Width (CM)");   
+        .text("Student Count (#)");   
     
     let y = d3.scaleLinear()
                 .range([height, 0]) //output
-                .domain([0, 3]); //input
+                .domain([0, 4]); //input
     
     
     //Appending y-axis to chart and labeling it 
@@ -191,144 +175,11 @@ function load_iris_visualization2(svg_name, data){
         .attr("font-size", "15px")
         .attr("font-weight", "bold")
         .attr("transform", "rotate(-90)")
-        .attr("x", -150)
+        .attr("x", -225)
         .attr("y", -50)
-        .text("Petal Width (CM)");
+        .text("GPA");
     
-    //Hue Scale
-    let hue = d3.scaleOrdinal()
-                       .domain(["setosa", "versicolor", "virginica" ])
-                       .range([ "#50C75D", "#C750B5", "#50C7C7"])
-
-
- 
-
-    //Interactivity Functions (highlighting & unhighlighting specific cluster of data)
-
-    //This function is to highlight the selected cluster of data.
-    var highlighting  = function(d){
-
-        iris_category = d.class
-
-        d3.selectAll("circles")
-            .transition()
-            .duration(300)
-            .style("fill", "black")
-            .attr("r", 5)
-        
-        d3.selectAll("." + iris_category)
-            .transition()
-            .duration(300)
-            .style("fill", hue(iris_category))
-            .attr("r", 5)
-    }
-
-    //This function is to (UN)highlight clusters of data.
-    var unhighlight = function(){
-        d3.selectAll(".points")
-            .transition()
-            .duration(100)
-            .style("fill", "black")
-            .attr("r", 5)
-    }
-    
-    //Creating the "dots" of the scatter plot
-    g.selectAll("circles")
-         .data(data)
-         .enter()
-         .append("circle")
-            .attr("class", function (d) { return "points " + d.class } )
-            .attr("cx",function(d){ return x(d.sepal_width); })
-            .attr("cy",function(d){ return y(d.petal_width); })
-            .attr("r", 5)
-            .style("fill", function (d) { return hue(d.class) } )
-         .on("mouseover",highlighting)
-         .on("mouseleave", unhighlight)
-    
-    
-    //Creating the legend for this visualization
-    g.append("circle").attr("cx",680).attr("cy",130).attr("r", 7).style("fill", "#50C75D")
-    g.append("circle").attr("cx",680).attr("cy",160).attr("r", 7).style("fill", "#C750B5")
-    g.append("circle").attr("cx",680).attr("cy",190).attr("r", 7).style("fill", "#50C7C7")
-    g.append("text").attr("x", 700).attr("y", 100).text("LEGEND").style("font-size", "15px").style("font-weight", "bold").attr("alignment-baseline","middle")
-    g.append("text").attr("x", 700).attr("y", 130).text("iris-setosa").style("font-size", "15px").attr("alignment-baseline","middle")
-    g.append("text").attr("x", 700).attr("y", 160).text("iris-versicolor").style("font-size", "15px").attr("alignment-baseline","middle")
-    g.append("text").attr("x", 700).attr("y", 190).text("iris-virginica").style("font-size", "15px").attr("alignment-baseline","middle")
-    
-    
-}
-
-function load_iris_visualization3(svg_name, data, tip){
-
-    //console.log("HEREREER");
-    let chart = d3.select(svg_name);
-    let margin2 = 50;
-    let width = $(svg_name).width() - margin2;
-    let height = $(svg_name).height()- margin2;
-
-    //grouping
-    //let g = chart.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    let g = chart.append("g")
-                .attr("transform", "translate(" + 55 + "," + 3 + ")");
-
-    //Adding Label to chart (title)
-    g.append("text")
-        .attr("text-anchor", "end")
-        .attr("font-size", "15px")
-        .attr("font-weight", "bold")
-        .attr("x", 600)
-        .attr("y", 20)
-        .text("Distribution of Sepal Length Amongst Iris Plants");
-            
-    //x,y scales
-    //x axis
-    let x = d3.scaleLinear()
-                .range([0, width])//output
-                .domain([4, 10]);//input
-
-    //Appending x-axis to chart and labeling it 
-    g.append("g")
-        .attr("transform", "translate(0," + height+")")
-        .call(d3.axisBottom(x));
-        
-    g.append("text")
-        .attr("class", "y label")
-        .attr("text-anchor", "end")
-        .attr("font-size", "15px")
-        .attr("font-weight", "bold")
-        .attr("x", width-400)
-        .attr("y", height+35)
-        .text("Sepal Length(CM)");
-
-
-
-    //Creating histogram model & Bins
-    var histogram = d3.histogram()
-        .value(function(data) {return data.sepal_length;})
-        .domain(x.domain())
-        .thresholds(x.ticks(40));
-    
-
-    var bin = histogram(data);
-    
-    
-    //y axis
-    let y = d3.scaleLinear()
-                .range([height, 0]) //output
-                .domain([0,20]); //input
-    
-    //Appending y-axis to chart and labeling it 
-    g.append("g").call(d3.axisLeft(y));
-
-    g.append("text")
-        .attr("text-anchor", "end")
-        .attr("font-size", "15px")
-        .attr("font-weight", "bold")
-        .attr("transform", "rotate(-90)")
-        .attr("x", -height + 300)
-        .attr("y", -40)
-        .text("Number of Iris-Setosa Plants");
-
+    //IMPLEMENTATION OF TOOL TIP FUNCTIONALITY
     //Creating tooltip
     var tooltip = tip;
         tooltip.attr("class", "d3-tip")
@@ -344,8 +195,141 @@ function load_iris_visualization3(svg_name, data, tip){
                 );
 
     //Calling our tooltip to be used on the Visualization
-    g.call(tooltip);              
-    //appending data from bin to the histogram
+    g.call(tooltip);
+
+
+    //Creating the "dots" of the scatter plot
+    g.selectAll("circles")
+         .data(data)
+         .enter()
+         .append("circle")
+            .attr("cx",function(d,i){ return x(i); })
+            .attr("cy",function(d){ return y(d.gpa_data); })
+            .attr("r", 4)
+            .style("fill", "#50C7C7")
+            .style("hover", "stroke: #000000")
+            //Defining mouseover function for tooltip
+            .on("mouseover", function(d){
+                d3.select(this).style("fill", "red");
+                curr_position = d3.mouse(this); 
+                curr_state = d.gpa_data;
+
+                //Show the tooltip
+                tooltip.show();
+
+                //Designing our tooltip via 'toolTipDiv'
+                var tool_tip_design = d3.select("#toolTipDiv")
+                                        .append("svg")
+                                            .attr("width", 500)
+                                            .attr("height", 55);
+
+                tool_tip_design.append("text")
+                                .text("GPA: " +d.gpa_data)
+                                .style("fill", "red")
+                                .style("font-weight", "bold")
+                                .attr("x", 10)
+                                .attr("y", 10)
+            })
+            //Defining mouseout function for tooltip
+            .on("mouseout", function(d) {
+
+                d3.select(this).style("fill", "#50C7C7");
+                tooltip.hide();
+            })
+    
+
+
+
+}
+
+function load_calvin_visualization3(svg_name, data,tip_two){
+
+    //console.log("HEREREER");
+    let chart = d3.select(svg_name);
+    let margin2 = 50;
+    let width = $(svg_name).width() - margin2;
+    let height = $(svg_name).height()- margin2;
+
+    //grouping
+    //let g = chart.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    let g = chart.append("g")
+                .attr("transform", "translate(" + 55 + "," + 3 + ")");
+
+    //Adding Label to chart (title)
+    g.append("text")
+        .attr("text-anchor", "end")
+        .attr("font-size", "20px")
+        .attr("font-weight", "bold")
+        .attr("x", 250)
+        .attr("y", 20)
+        .text("ACT Score Distribution");
+            
+    //x,y scales
+    //x axis
+    let x = d3.scaleLinear()
+                .range([0, width])//output
+                .domain([10, 37]);//input
+                
+
+
+    //Appending x-axis to chart and labeling it 
+    g.append("g")
+        .attr("transform", "translate(0," + height+")")
+        .call(d3.axisBottom(x));
+        
+    g.append("text")
+        .attr("class", "y label")
+        .attr("text-anchor", "end")
+        .attr("font-size", "15px")
+        .attr("font-weight", "bold")
+        .attr("x", width-400)
+        .attr("y", height+35)
+        .text("Score on ACT");
+
+    //Creating histogram model & Bins
+
+    var histogram = d3.histogram()
+        .value(function(data) {return data.act_data;})
+        .domain(x.domain())
+        .thresholds(x.ticks(40));
+    
+    var bin = histogram(data);
+        
+    //y axis
+    let y = d3.scaleLinear()
+                .range([height, 0]) //output
+                .domain([0,d3.max(bin, function(d){return d.length;})]); //input
+    
+    //Appending y-axis to chart and labeling it 
+    g.append("g").call(d3.axisLeft(y));
+
+    g.append("text")
+        .attr("text-anchor", "end")
+        .attr("font-size", "15px")
+        .attr("font-weight", "bold")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -height + 250)
+        .attr("y", -40)
+        .text("Number of People");
+
+
+    //Creating tooltip
+    var tooltipTwo = tip_two;
+        tooltipTwo.attr("class", "d3-tip2")
+               .offset(function() {
+                   if(curr_position[0] > 650) {
+                       return [-10,50] 
+                    } 
+                    else { 
+                        return [10,10]
+                    }
+                }).html(
+                    "<div id='toolTipDivision'></div>"
+                );
+    //Calling our tooltip to be used on the Visualization
+    g.call(tooltipTwo);
+
+    //appending bars to the histogram
     g.selectAll("histogram")
             .data(bin)
             .enter()
@@ -354,25 +338,25 @@ function load_iris_visualization3(svg_name, data, tip){
                .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
                .attr("width", 10)
                .attr("height", function(d) { return height - y(d.length); })
-               .style("fill", "#C78B50")
+               .style("fill", "#69b3a2")
                //Defining mouseover function for tooltip
                .on("mouseover", function(d,i){
                     d3.select(this).style("fill", "red");
                     curr_position = d3.mouse(this); 
                     curr_state = d.act_data;
-                    num_plants = d.length;
+                    num_people = d.length;
                     //Show the tooltip
-                    tooltip.show();
+                    tooltipTwo.show();
 
                     //Designing our tooltip via 'toolTipDiv'
-                    var tool_tip_design = d3.select("#toolTipDiv")
+                    var tool_tip_design_two = d3.select("#toolTipDivision")
                                                 .append("svg")
                                                     .attr("width", 500)
                                                     .attr("height", 55);
 
-                    tool_tip_design.append("text")
-                                        .text("# iris setosa plant: " + num_plants)
-                                        .style("fill", "green")
+                    tool_tip_design_two.append("text")
+                                        .text("# of people: " + num_people)
+                                        .style("fill", "red")
                                         .style("font-weight", "bold")
                                         .attr("x", 10)
                                         .attr("y", 10)
@@ -380,12 +364,7 @@ function load_iris_visualization3(svg_name, data, tip){
             //Defining mouseout function for tooltip
             .on("mouseout", function(d) {
 
-                d3.select(this).style("fill", "#C78B50");
-                tooltip.hide();
+                d3.select(this).style("fill", "#69b3a2");
+                tooltipTwo.hide();
             })
 }
-
-
-
-
-
